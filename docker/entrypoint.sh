5ccaf4e1d828cb784aebf4a894b5e5f84774ec89
dungviettran89 <dungviettran89@gmail.com>
2019-10-26 12:37:30 +0800

    Auto restart chrome

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -15,5 +15,5 @@ export CHROME_FLAGS="$CHROME_FLAGS --window-position=0,0"
 sleep 2s
 /usr/bin/x11vnc -rfbport 5900 -xkb -noxrecord -noxfixes -noxdamage -display :99 -wait 5 -shared -forever > >(sed 's/^/[x11vnc] /') 2>&1  &
 /usr/bin/novnc_server --listen 6080 --vnc localhost:5900 > >(sed 's/^/[novnc_server] /') 2>&1  &
-/usr/bin/chromium-browser ${CHROME_FLAGS} > >(sed 's/^/[chromium] /') 2>&1  &
+while true; do /usr/bin/chromium-browser ${CHROME_FLAGS}; sleep 10s; done > >(sed 's/^/[chromium] /') 2>&1 &
 wait
