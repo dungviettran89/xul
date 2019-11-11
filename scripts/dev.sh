107c186dfcbdeb54ee8749e9ea8c8db735b4a6bd
Dung Tran <dungviettran89@gmail.com>
2019-11-11 09:57:42 +0800

    Renamed to xul a famous Necromancer which can raise an undead army to do his bidding

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 set -e
 cd ..
-if docker start grid-server-database; then
+if docker start xul-database; then
     echo "MariaDB is running";
 else
-    docker network create --driver bridge grid-server || true
-    docker run --name grid-server-database \
-    -d --network=grid-server \
+    docker network create --driver bridge xul-network || true
+    docker run --name xul-database \
+    -d --network=xul-network \
     -p 3306:3306 \
     -e MYSQL_ROOT_PASSWORD=automation \
     -e MYSQL_USER=automation \
     -e MYSQL_PASSWORD=automation \
     -e MYSQL_DATABASE=automation \
     mariadb:10.4 || true
-    timeout 5m docker logs -f grid-server-database || true
+    timeout 5m docker logs -f xul-database || true
 fi
-docker rm -f k8s-cucumber-grid-1 k8s-cucumber-grid-2  || true
-docker build -f docker/Dockerfile -t k8s-cucumber-grid:dev .
+docker rm -f xul-server-1 xul-server-2  || true
+docker build -f docker/Dockerfile -t xul-server:dev .
 
 _term() {
   echo "Stopping all child!"
-  docker rm -f k8s-cucumber-grid-1 k8s-cucumber-grid-2  || true
+  docker rm -f xul-server-1 xul-server-2  || true
   exit 1
 }
 
 trap _term SIGTERM
-export SHARED_PARAMS=" -e db_host=grid-server-database --network=grid-server "
-docker run ${SHARED_PARAMS} --name k8s-cucumber-grid-2 k8s-cucumber-grid:dev &
-docker run ${SHARED_PARAMS} --name k8s-cucumber-grid-1 -p 6080:6080 -p 8080:8080 k8s-cucumber-grid:dev &
+export SHARED_PARAMS=" -e db_host=xul-database --network=xul-network "
+docker run ${SHARED_PARAMS} --name xul-server-2 xul-server:dev &
+docker run ${SHARED_PARAMS} --name xul-server-1 -p 6080:6080 -p 8080:8080 xul-server:dev &
 wait
