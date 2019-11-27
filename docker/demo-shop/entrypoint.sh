d3ce768d018209a3a406fef16a1ca8b0b47e3814
dungviettran89 <dungviettran89@gmail.com>
2019-11-27 14:02:43 +0000

    Fixed user host

--- docker/demo-shop/entrypoint.sh
+++ docker/demo-shop/entrypoint.sh
@@ -2,8 +2,8 @@
 preparedb(){
     mysql -uroot -h127.0.0.1 test -e "FLUSH PRIVILEGES;"
     mysql -uroot -h127.0.0.1 test -e "CREATE DATABASE presta_demo;"
-    mysql -uroot -h127.0.0.1 test -e "CREATE USER 'presta_demo'@'%' IDENTIFIED BY 'presta_demo';"
-    mysql -uroot -h127.0.0.1 test -e "GRANT ALL PRIVILEGES ON *.* TO 'presta_demo'@'%';"
+    mysql -uroot -h127.0.0.1 test -e "CREATE USER 'presta_demo'@'localhost' IDENTIFIED BY 'presta_demo';"
+    mysql -uroot -h127.0.0.1 test -e "GRANT ALL PRIVILEGES ON *.* TO 'presta_demo'@'localhost';"
 }
 sleep 20s && preparedb && /tmp/docker_run.sh &
 ./mysql.sh
