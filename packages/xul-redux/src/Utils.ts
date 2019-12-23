1d2424297b8cd4c5c62146bdf00eeb308ac6c968
Tran Viet Dung <dungviettran89@gmail.com>
2019-12-23 23:02:44 +0800

    Fixed tslint issue

--- packages/xul-redux/src/Utils.ts
+++ packages/xul-redux/src/Utils.ts
@@ -2,7 +2,7 @@ export const get = (obj: any, path: string, defaultValue?: any): any => {
   const result = String.prototype.split
     .call(path, /[,[\].]+?/)
     .filter(Boolean)
-    .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
+    .reduce((res: any, key: any) => (res !== null && res !== undefined ? res[key] : res), obj);
   return result === undefined || result === obj ? defaultValue : result;
 };
 export const lowerFirst = (name: string): string => {
