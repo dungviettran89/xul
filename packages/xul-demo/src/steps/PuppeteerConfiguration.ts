e43b0c3f3a349922478656ced611bff2625a7c30
dungviettran89 <dungviettran89@gmail.com>
2020-04-07 23:58:39 +0800

    Temporary delete steps

--- packages/xul-demo/src/steps/PuppeteerConfiguration.ts
+++
@@ -1,9 +0,0 @@
-import { singleton } from "@xul/core";
-import { IPuppeteerConfiguration } from "@xul/test";
-import { setDefaultTimeout } from "cucumber";
-@singleton()
-export class PuppeteerConfiguration implements IPuppeteerConfiguration {
-  public width: number = 1024;
-  public height: number = 576;
-}
-setDefaultTimeout(20000);
--- packages/xul-demo/src/steps/Utils.ts
+++
@@ -1,6 +0,0 @@
-export const sameString = (s1: string, s2: string): boolean => {
-  if (s1 === undefined || s2 === undefined) {
-    return s1 === s2;
-  }
-  return s1.trim().toLowerCase() === s2.trim().toLowerCase();
-};
